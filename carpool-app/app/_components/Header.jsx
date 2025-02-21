ec6b8aff1ab156fdd3927df630deac2981064ea2
Trang Vu <106600297+Trangvu801@users.noreply.github.com>
2025-02-20 21:49:54 -0500

    autho, supabase setup

--- carpool-app/app/_components/Header.jsx
+++ carpool-app/app/_components/Header.jsx
@@ -1,29 +1,31 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 function Header() {
   const path = usePathname();
+  const { user, isSignedIn } = useUser();
 
   useEffect(() => {
     console.log(path);
-  }, [path]);  // Track pathname change on each update
+  }, [path]); // Track pathname change on each update
 
   return (
     <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-12 items-center">
-        <Image src={'/car.svg'} width={100} height={100} alt="car icon" />
+        <Image src={"/car.svg"} width={100} height={100} alt="car icon" />
         <ul className="hidden md:flex gap-10">
           {/* Ride Offer Link */}
-          <Link href={'/'}>
+          <Link href={"/"}>
             <li
               className={`hover:text-primary font-medium text-sm cursor-pointer ${
-                path === '/' ? 'text-primary' : ''
+                path === "/" ? "text-primary" : ""
               }`}
             >
               Ride Offer
@@ -31,10 +33,10 @@ function Header() {
           </Link>
 
           {/* Ride Request Link */}
-          <Link href={'/ride-request'}>
+          <Link href={"/ride-request"}>
             <li
               className={`hover:text-primary font-medium text-sm cursor-pointer ${
-                path === '/ride-request' ? 'text-primary' : ''
+                path === "/ride-request" ? "text-primary" : ""
               }`}
             >
               Ride Request
@@ -42,10 +44,10 @@ function Header() {
           </Link>
 
           {/* Agent Finder Link */}
-          <Link href={'/agent-finder'}>
+          <Link href={"/agent-finder"}>
             <li
               className={`hover:text-primary font-medium text-sm cursor-pointer ${
-                path === '/agent-finder' ? 'text-primary' : ''
+                path === "/agent-finder" ? "text-primary" : ""
               }`}
             >
               Agent Finder
@@ -57,7 +59,11 @@ function Header() {
         <Button className="flex gap-2">
           <Plus className="h-5 w-5" /> Post Your Ride
         </Button>
+        {isSignedIn ? <UserButton /> :
+        <Link href={'/sign-in'}>
         <Button variant="outline">Login</Button>
+        </Link>
+        }
       </div>
     </div>
   );
